7f6ddb3a1c375b438d6e1daf7552b7a2d11ada91
Ian Oakes <ian.oakes@trillian.com.au>
2018-09-05 15:27:59 +1000

    initial commit

---
+++ src/BureauOfMeteorology.Model/Observation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BureauOfMeteorology.Model
+{
+    public class Observation
+    {
+        public string ProductId { get; set; }
+
+        public DateTime LocalDateTime { get; set; }
+
+        public decimal Temperature { get; set; }
+
+        public decimal ApparentTemperature { get; set; }
+
+        public decimal DewPoint { get; set; }
+
+        public decimal AirPressure { get; set; }
+
+        public int RelativeHumidity { get; set; }
+
+        public decimal Rain { get; set; }
+
+        public int WindSpeedKmh { get; set; }
+
+        public int WindSpeedKnots { get; set; }
+
+        public int WindGustKmh { get; set; }
+
+        public int WindGustKnots { get; set; }
+
+        public string WindDirection { get; set; }
+
+    }
+}
---
+++ src/BureauOfMeteorology.Model/StationObservation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace BureauOfMeteorology.Model
+{
+    public class StationObservation
+    {
+        public string StationName { get; set; }
+
+        public string StationId { get; set; }
+
+        public decimal Lattitude { get; set; }
+
+        public decimal Longitude { get; set; }
+
+        public List<Observation> Observations { get; set; } = new List<Observation>();
+    }
+}
